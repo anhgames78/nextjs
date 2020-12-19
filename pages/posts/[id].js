67539621f72cc38eac9735d6730cc4e0c148679f
anhgames78 <35523549+anhgames78@users.noreply.github.com>
2020-12-19 12:00:37 +0700

    Update [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,11 @@
 // pages/posts/[id].js
 
+import Link from '../../src/Link';
+import Copyright from '../../src/Copyright';
+
+const api = "https://anhgames78.github.io/public/";
+const DATA = api + "data.json";
+
 function Post({ post }) {
   // Render post...
 }
@@ -7,28 +13,26 @@ function Post({ post }) {
 // This function gets called at build time
 export async function getStaticPaths() {
   // Call an external API endpoint to get posts
-  const res = await fetch('https://.../posts')
-  const posts = await res.json()
-
+  const response = await fetch(DATA);
+  const posts = await res.json();
+  const ids = Object.keys(posts); // get obj key from posts as ["0","1","2",...]
   // Get the paths we want to pre-render based on posts
-  const paths = posts.map((post) => ({
-    params: { id: post.id },
-  }))
-
+  const paths = ids.map((key) => ({
+    params: { id: key },
+  }));
+  
   // We'll pre-render only these paths at build time.
   // { fallback: false } means other routes should 404.
-  return { paths, fallback: false }
+  return { paths, fallback: false };
 }
 
 // This also gets called at build time
-export async function getStaticProps({ params }) {
-  // params contains the post `id`.
-  // If the route is like /posts/1, then params.id is 1
-  const res = await fetch(`https://.../posts/${params.id}`)
-  const post = await res.json()
-
-  // Pass post data to the page via props
-  return { props: { post } }
+export async function getStaticProps(context) {
+  const { id } = context.params;
+  const response = await fetch(DATA);
+  const posts = await response.json();
+  const post = posts[name];
+  return { props: { post } };
 }
 
 export default Post

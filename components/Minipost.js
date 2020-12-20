de7f66efc4f85e3b67caf3cab76ad2e214169121
anhgames78 <35523549+anhgames78@users.noreply.github.com>
2020-12-20 21:50:51 +0700

    Update Minipost.js

--- components/Minipost.js
+++ components/Minipost.js
@@ -1,13 +1,14 @@
 import Link from '../src/Link';
 
 const Minipost = (props) => {
+  const address = "/posts/" + props.id.toString();
   return (
     <div className="col-4">
       <div className="card-deck">
         <div className="card">
-          <Link href="/posts/{props.id.toString()}"><img className="card-img-top" src="http://placehold.it/800x400" alt="Card image cap" /></Link>
+          <Link href={address}><img className="card-img-top" src="http://placehold.it/800x400" alt="Card image cap" /></Link>
           <div className="card-body">
-            <Link href="/posts/{props.id.toString()}" className="card-title">{props.title}</Link>
+            <Link href={address} className="card-title">{props.title}</Link>
             <p className="card-text">{props.summary}</p>
             <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
           </div>

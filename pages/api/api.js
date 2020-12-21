df257c18c25dfaa8380b924d9ba8633c5d7914f2
anhgames78 <35523549+anhgames78@users.noreply.github.com>
2020-12-21 13:00:58 +0700

    Create api.js

---
+++ pages/api/api.js
@@ -0,0 +1,23 @@
+//API to calculate different between two date object
+export function time_diff(date1,date2)
+{
+    // get total seconds between the times
+    var delta = Math.abs(date1 - date2) / 1000;
+
+    // calculate (and subtract) whole days
+    var days = Math.floor(delta / 86400);
+    delta -= days * 86400;
+
+    // calculate (and subtract) whole hours. 
+    var hours = Math.floor(delta / 3600);
+    delta -= hours * 3600;
+
+    // calculate (and subtract) whole minutes
+    var minutes = Math.floor(delta / 60);
+    delta -= minutes * 60;
+
+    // what's left is seconds
+    var seconds = delta;
+    
+    return [days,hours,minutes,seconds];
+}
